c692e5f31a1a224862c1c82aaff7912692a11d98
Joel Midstjärna <joel.midstjarna@gmail.com>
2019-01-21 19:24:13 +0100

    Now possible to view images

--- media.go
+++ media.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"io/ioutil"
-	"path"
+	"path/filepath"
+	"strings"
 )
+
+var imgExtensions = [...]string{".png", ".jpg", ".jpeg", ".tiff", ".gif"}
+var vidExtensions = [...]string{".avi", ".mov", ".vid"}
 
 // Media represents the media including its base path
 type Media struct {
 	basePath string
 }
 
-// File represents a directory or any other file
+// File represents a folder or any other file
 type File struct {
-	IsDir bool
-	Name  string
-	Path  string // Including Name
+	Type string // folder, image or video
+	Name string
+	Path string // Including Name
 }
 
 func createMedia(basePath string) *Media {
@@ -25,18 +29,45 @@ func createMedia(basePath string) *Media {
 func (m *Media) getFiles(relativePath string) ([]File, error) {
 	//var files []File
 	files := make([]File, 0, 500)
-	fullPath := path.Join(m.basePath, relativePath)
+	fullPath := filepath.Join(m.basePath, relativePath)
 	fileInfo, err := ioutil.ReadDir(fullPath)
 	if err != nil {
 		return files, err
 	}
 
 	for _, fileInfo := range fileInfo {
-		file := File{
-			IsDir: fileInfo.IsDir(),
-			Name:  fileInfo.Name(),
-			Path:  path.Join(relativePath, fileInfo.Name())}
-		files = append(files, file)
+		fileType := ""
+		if fileInfo.IsDir() {
+			fileType = "folder"
+		} else {
+			extension := filepath.Ext(fileInfo.Name())
+
+			// Check if this is an image
+			for _, imgExtension := range imgExtensions {
+				if strings.EqualFold(extension, imgExtension) {
+					fileType = "image"
+					break
+				}
+			}
+
+			// Check if this is a video
+			if fileType == "" {
+				for _, vidExtension := range vidExtensions {
+					if strings.EqualFold(extension, vidExtension) {
+						fileType = "video"
+						break
+					}
+				}
+			}
+		}
+		// Only add directories, videos and images
+		if fileType != "" {
+			file := File{
+				Type: fileType,
+				Name: fileInfo.Name(),
+				Path: filepath.Join(relativePath, fileInfo.Name())}
+			files = append(files, file)
+		}
 	}
 	return files, nil
 }
