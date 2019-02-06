1236e51a80ef26ad0eab45b459e1e787b48f96a2
Joel Midstjärna <joel.midstjarna@gmail.com>
2019-02-06 20:39:14 +0100

    Added version and appveyor script

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	if s.logFile != "" {
 		llog.SetFile(s.logFile, 1024) // 1 MB logs
 	}
+	llog.Info("Version: %s", applicationVersion)
+	llog.Info("Build time: %s", applicationBuildTime)
+	llog.Info("Git hash: %s", applicationGitHash)
 	media := createMedia(s.mediaPath, s.thumbPath, s.enableThumbCache, s.autoRotate)
 	box := packr.New("templates", "./templates")
 	webAPI := CreateWebAPI(s.port, "templates", media, box)
