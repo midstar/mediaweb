fb40e3f740781f59cf310cd2c475e240e577b22c
Joel Midstjärna <joel.midstjarna@gmail.com>
2019-04-11 15:22:28 +0200

    Added fsnotify to dependencies

--- scripts/install_deps.bat
+++ scripts/install_deps.bat
@@ -4,4 +4,5 @@ go get github.com/disintegration/imaging
 go get github.com/rwcarlsen/goexif/exif
 go get github.com/GeertJohan/go.rice
 go get github.com/GeertJohan/go.rice/rice
+go get github.com/fsnotify/fsnotify
 go get github.com/kardianos/service
