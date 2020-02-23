ede9310c7e8069fbecbbb11c454ff3293270f2f0
Joel Midstjärna <joel.midstjarna@gmail.com>
2020-02-23 20:30:37 +0100

    Fixed file permissions

--- scripts/debian_installer.sh
+++ scripts/debian_installer.sh
@@ -4,14 +4,6 @@ set -e
 # MediaWEB Linux Debian package creator shell script
 #
 # See print_usage below for instructions
-export SCRIPT_PATH=`dirname "$0"`
-export PKG_TEMPLATE_PATH=$SCRIPT_PATH/debian_pkg
-export PKG_PATH=$SCRIPT_PATH/tmp_debian_pkg
-export PKG_APP_PATH=$PKG_PATH/app_tmp
-export MEDIAWEB_EXE=$SCRIPT_PATH/../mediaweb
-export MEDIAWEB_CFG=$SCRIPT_PATH/../configs/mediaweb.conf
-export PACKAGE_DESTINATION=$SCRIPT_PATH/..
-
 
 print_usage() {
     echo "Usage:"
@@ -28,6 +20,13 @@ print_usage() {
     echo "MediaWEB exectuable for the correct architecture needs"
     echo "to be built and but directly in the mediaweb path."
     exit 1
+}
+
+copy_w_permission() {
+    DIR=`dirname "$2"`
+    mkdir -p $DIR
+    cp $1 $2
+    chmod $3 $2
 }
 
 if [ ! -f $MEDIAWEB_EXE ]; then
@@ -47,64 +46,79 @@ export ARCHITECTURE=$1
 export VERSION=$2
 export NAME=$3
 
+# Create package source create directory
+export SCRIPT_PATH=`dirname "$0"`
+export PKG_IN_PATH=$SCRIPT_PATH/debian_pkg
+export PKG_OUT_PATH=$SCRIPT_PATH/tmp_debian_pkg/$NAME
+export PKG_ROOT_PATH=$PKG_OUT_PATH/root
+export MEDIAWEB_EXE=$SCRIPT_PATH/../mediaweb
+export MEDIAWEB_CFG=$SCRIPT_PATH/../configs/mediaweb.conf
+
+export PACKAGE_DESTINATION=$SCRIPT_PATH/..
+
 # Cleanup - remove previous package path if it exist
-if [ -d $PKG_PATH ]; then
-    rm -Rf $PKG_PATH
+if [ -d $PKG_OUT_PATH ]; then
+    rm -Rf $PKG_OUT_PATH
 fi
 
-echo "Listing permissions 1"
-ls -Rlh $SCRIPT_PATH
+# ----------------------------------------------
+# COPY IN TO OUT
+cp -r $PKG_IN_PATH $PKG_OUT_PATH 
+# Set general permissions
+find $PKG_OUT_PATH -type d -exec chmod 755 {} +
+find $PKG_OUT_PATH  -type f -exec chmod 644 {} +
 
 
-# Create package source create directory
-export PKG_SRC_PATH=$PKG_PATH/$NAME
+# ----------------------------------------------
+# FILL ROOT DIRECTORY
 
-# Copy template files
-mkdir -p $PKG_SRC_PATH
-cp -r $PKG_TEMPLATE_PATH/DEBIAN $PKG_SRC_PATH
+# Copy mediaweb executable and modify permissions
+copy_w_permission $MEDIAWEB_EXE $PKG_ROOT_PATH/usr/sbin/mediaweb 755
+strip --strip-unneeded $PKG_ROOT_PATH/usr/sbin/mediaweb
 
-# Copy files to install to tmp
-mkdir -p $PKG_APP_PATH/usr/sbin
-cp $MEDIAWEB_EXE $PKG_APP_PATH/usr/sbin/
-strip --strip-unneeded $PKG_APP_PATH/usr/sbin/mediaweb
+# Copy mediaweb configuration and modify permissions
+copy_w_permission $MEDIAWEB_CFG $PKG_ROOT_PATH/etc/mediaweb.conf 644
 
-cp -r $PKG_TEMPLATE_PATH/usr $PKG_APP_PATH
-cp -r $PKG_TEMPLATE_PATH/lib $PKG_APP_PATH
-mkdir $PKG_APP_PATH/etc/
-cp $MEDIAWEB_CFG $PKG_APP_PATH/etc/
-
-# Figure out size of application
-SIZE=$(du -s ./$PKG_APP_PATH | awk '{print $1}')
-
-# Move files to install to src
-mv $PKG_APP_PATH/* $PKG_SRC_PATH
-rm -Rf $PKG_APP_PATH
-
-# Modify control file with version and architecture
-sed -i -e 's/__ARCHITECTURE__/'${ARCHITECTURE}'/g' $PKG_SRC_PATH/DEBIAN/control
-sed -i -e 's/__VERSION__/'${VERSION}'/g' $PKG_SRC_PATH/DEBIAN/control
-sed -i -e 's/__SIZE__/'${SIZE}'/g' $PKG_SRC_PATH/DEBIAN/control
+# Compress the man page and modify permissions
+gzip -n --best $PKG_ROOT_PATH/usr/share/man/man1/mediaweb.1
 
 # Create changelog
-export CHANGELOG_PATH=$PKG_SRC_PATH/usr/share/doc/mediaweb
-sh $SCRIPT_PATH/generate_changelog.sh mediaweb $VERSION mediaweb-v $CHANGELOG_PATH/changelog
-gzip -n --best $CHANGELOG_PATH/changelog
+export CHANGELOG=$PKG_ROOT_PATH/usr/share/doc/mediaweb/changelog
+sh $SCRIPT_PATH/generate_changelog.sh mediaweb $VERSION mediaweb-v $CHANGELOG
+gzip -n --best $CHANGELOG
 
-# Compress the man page
-gzip -n --best $PKG_SRC_PATH/usr/share/man/man1/mediaweb.1
+# Calculate size of root directory
+SIZE=$(du -s ./$PKG_ROOT_PATH | awk '{print $1}')
 
-# Create the installer 
-dpkg-deb --build $PKG_SRC_PATH
+
+# ----------------------------------------------
+# DEBIAN DIRECTORY
+
+# Modify control file with version, architecture and size
+sed -i -e 's/__ARCHITECTURE__/'${ARCHITECTURE}'/g' $PKG_OUT_PATH/DEBIAN/control
+sed -i -e 's/__VERSION__/'${VERSION}'/g' $PKG_OUT_PATH/DEBIAN/control
+sed -i -e 's/__SIZE__/'${SIZE}'/g' $PKG_OUT_PATH/DEBIAN/control
+
+# Set premissions on scripts
+chmod 755 $PKG_OUT_PATH/DEBIAN/post* $PKG_OUT_PATH/DEBIAN/pre*
+
+
+# ----------------------------------------------
+# MOVE ROOT FILES AND REMOVE ROOT DIRECTORY
+
+mv $PKG_ROOT_PATH/* $PKG_OUT_PATH
+rm -Rf $PKG_ROOT_PATH
+
+# ----------------------------------------------
+# CREATE THE INSTALLER
+
+# Create
+dpkg-deb --build $PKG_OUT_PATH
 
 # Move the resulting installer to MediaWEB root folder
-mv $PKG_PATH/${NAME}.deb $PACKAGE_DESTINATION/
+mv ${PKG_OUT_PATH}.deb $PACKAGE_DESTINATION/
 echo Generated:
 realpath $PACKAGE_DESTINATION/${NAME}.deb
-
-
-echo "Listing permissions 1"
-ls -Rlh $SCRIPT_PATH
-
 
 # Check with lintian
 echo Validating package:
