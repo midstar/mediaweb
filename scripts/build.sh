c747c7a71f3adf3bb95722c08d972023c062d20a
Joel Midstjärna <joel.midstjarna@gmail.com>
2020-06-01 19:56:41 +0200

    More error checks for rice

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,6 +15,9 @@ echo date time: $DATETIME
 
 echo building / installing
 cd $GOPATH/src/github.com/midstar/mediaweb
-rice embed-go
+rice embed-go || (
+  echo Go rice is not installed
+  exit 1
+)
 go build -ldflags="-s -X 'main.applicationBuildTime=$DATETIME' -X main.applicationVersion=$1 -X main.applicationGitHash=$GITHASH" github.com/midstar/mediaweb
 
